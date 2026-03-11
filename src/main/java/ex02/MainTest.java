2d2b5c24a370b4179c031a17d10077e3401cc75c
dranefor <savelamaksim011@gmail.com>
2026-03-11 13:47:01 +0200

    Implement main method for ViewResult testing
    Added main method to MainTest for testing ViewResult.

--- src/main/java/ex02/MainTest.java
+++ src/main/java/ex02/MainTest.java
@@ -1,4 +1,11 @@
 package ex02;
 
 public class MainTest {
+    public static void main(String[] args) {
+        ViewResult v = new ViewResult();
+        v.viewInit();
+        System.out.println("Testing initialization");
+        v.viewShow();
+        System.out.println("Test finished");
+    }
 }
